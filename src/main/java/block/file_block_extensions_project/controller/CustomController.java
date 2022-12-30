fb19511022fd8ae83edd06a0386cbbfd0e69e10a
leeseungwoo <vugil@naver.com>
2022-12-31 02:25:27 +0900

    feat : CustomExtension 삭제기능 구현

--- src/main/java/block/file_block_extensions_project/controller/CustomController.java
+++ src/main/java/block/file_block_extensions_project/controller/CustomController.java
@@ -6,10 +6,7 @@ import block.file_block_extensions_project.dto.response.CustomResponseDto;
 import block.file_block_extensions_project.service.CustomService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -23,8 +20,13 @@ public class CustomController {
 
     // 커스텀 확장자 추가
     @PostMapping("/register")
-    public ResponseDto<CustomResponseDto.CustomRegisterResponseDto> CustomRegister(@RequestBody @Valid CustomRequestDto.CustomRegisterRequestDto customRegisterRequestDto) {
+    public ResponseDto<CustomResponseDto.CustomRegisterResponseDto> customRegister(@RequestBody @Valid CustomRequestDto.CustomRegisterRequestDto customRegisterRequestDto) {
         return ResponseDto.success(customService.customRegister(customRegisterRequestDto));
+    }
+
+    @DeleteMapping("/{customId}")
+    public ResponseDto<CustomResponseDto.CustomRegisterResponseDto> customDelete(@PathVariable Long customId) {
+        return ResponseDto.success(customService.customDeleted(customId));
     }
 
 }
--- src/main/java/block/file_block_extensions_project/service/CustomService.java
+++ src/main/java/block/file_block_extensions_project/service/CustomService.java
@@ -4,6 +4,8 @@ import block.file_block_extensions_project.domain.CustomExtension;
 import block.file_block_extensions_project.dto.common.ResponseDto;
 import block.file_block_extensions_project.dto.request.CustomRequestDto;
 import block.file_block_extensions_project.dto.response.CustomResponseDto;
+import block.file_block_extensions_project.exception.CustomCommonException;
+import block.file_block_extensions_project.exception.ErrorCode;
 import block.file_block_extensions_project.repository.CustomRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +23,14 @@ public class CustomService {
     public CustomResponseDto.CustomRegisterResponseDto customRegister(CustomRequestDto.CustomRegisterRequestDto customRegisterRequestDto) {
         CustomExtension customExtension = new CustomExtension(customRegisterRequestDto);
         customRepository.save(customExtension);
+        return new CustomResponseDto.CustomRegisterResponseDto(customExtension);
+    }
+
+    public CustomResponseDto.CustomRegisterResponseDto customDeleted(Long customId) {
+        CustomExtension customExtension = customRepository.findById(customId).orElseThrow(
+                () -> new CustomCommonException(ErrorCode.CUSTOM_ID_FOUND)
+        );
+        customRepository.delete(customExtension);
         return new CustomResponseDto.CustomRegisterResponseDto(customExtension);
     }
 }
