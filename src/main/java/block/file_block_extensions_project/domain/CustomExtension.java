5772a6bdc52070e228d094482ce2eba82e83b543
leeseungwoo <vugil@naver.com>
2022-12-30 18:44:32 +0900

    Begin : Domain(Entity) 초기값 세팅

--- src/main/java/block/file_block_extensions_project/domain/CustomExtension.java
+++ src/main/java/block/file_block_extensions_project/domain/CustomExtension.java
@@ -1,4 +1,27 @@
 package block.file_block_extensions_project.domain;
 
-public class CustomExtension {
+import block.file_block_extensions_project.domain.base.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CustomExtension extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "custom_id")
+    private Long id;
+
+    @Column(name = "custom_name")
+    private String customExtensionName;
+
+    @Column(name = "custom_deleted")
+    private String isDeleted = "N";
 }
--- src/main/java/block/file_block_extensions_project/domain/FixedExtension.java
+++ src/main/java/block/file_block_extensions_project/domain/FixedExtension.java
@@ -1,4 +1,24 @@
 package block.file_block_extensions_project.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FixedExtension {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "fixed_id")
+    private Long id;
+
+    @Column(name = "fixed_name")
+    private String fixedExtensionName;
+
+    @Column(name = "fixed_checked")
+    private String isChecked = "N";
 }
