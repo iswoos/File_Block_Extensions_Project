ef1635166e99ecd85202c02c155469808ffe0363
leeseungwoo <vugil@naver.com>
2022-12-30 16:53:59 +0900

    Begin : 프로젝트 초기설정

---
+++ src/main/java/block/file_block_extensions_project/FileBlockExtensionsProjectApplication.java
@@ -0,0 +1,13 @@
+package block.file_block_extensions_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileBlockExtensionsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileBlockExtensionsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/block/file_block_extensions_project/FileBlockExtensionsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package block.file_block_extensions_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileBlockExtensionsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
