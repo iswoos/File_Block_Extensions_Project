18eb0c66bb30d6fd57f3de9957c357c98331d4c1
leeseungwoo <vugil@naver.com>
2022-12-31 11:27:52 +0900

    Update README.md

--- src/main/java/block/file_block_extensions_project/controller/FixedController.java
+++ src/main/java/block/file_block_extensions_project/controller/FixedController.java
@@ -1,4 +1,18 @@
 package block.file_block_extensions_project.controller;
 
+import block.file_block_extensions_project.repository.FixedRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/extensions")
 public class FixedController {
+
+    private final FixedRepository fixedRepository;
+
+
 }
